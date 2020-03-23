023c5a1d91751900d51b1d083365e1aa0905d1d8
shitalSonwane <shitals177@gmail.com>
2020-03-23 20:24:24 +0530

    Added:Check Mood Happy or Sad

--- src/main/java/MoodAnalyser.java
+++ src/main/java/MoodAnalyser.java
@@ -2,4 +2,10 @@ public class MoodAnalyser {
     public static void main(String args[]){
         System.out.println("Welcome to Mood Analyser ");
     }
+    public String analyseMood(String message) {
+        if (message.contains("sad"))
+            return "SAD";
+        else
+            return "HAPPY";
+    }
 }
--- src/test/java/TestMoodAnalyser.java
+++ src/test/java/TestMoodAnalyser.java
@@ -1,2 +1,18 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestMoodAnalyser {
+    MoodAnalyser MoodAnalyserObj=new MoodAnalyser();
+
+    @Test
+    public void givenMessage_whenResponse_thenSad() {
+        String result=MoodAnalyserObj.analyseMood("I am in sad Mood");
+        Assert.assertEquals("SAD",result);
+    }
+
+    @Test
+    public void givenMessage_whenReaponse_thenHappy() {
+        String result=MoodAnalyserObj.analyseMood("i am in any mood");
+        Assert.assertEquals("HAPPY",result);
+    }
 }
